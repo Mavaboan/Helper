5fbc8ae2352b0a911a8ec516333bc00745dd2610
PreussenBlue <bibbunk@gmail.com>
2019-06-03 11:02:43 +0200

    Added categories for budget

---
+++ Main.py
@@ -0,0 +1,19 @@
+inkomst = int(input("Hvor meget tjener du om måneden efter skat?"))
+
+husleje = int(input("Hvor meget bruger du på husleje?"))
+
+mad = int(input("Hvor meget bruger du på mad om måneden?"))
+
+utility = int(input("Hvor meget bruger du på el, vand, internet etc om måneden?"))
+
+underholdning = int(input("Hvor meget bruger du på underholdning om måneden?"))
+
+tøj = int(input("Hvor meget bruger du på tøj om måneden?"))
+
+opsparing = int(input("Hvor meget bruger du på din opsparing om måneden?"))
+
+forsikring = int(input("Hvor meget bruger du på forsikringer om måneden?"))
+
+transport = int(input("Hvor meget bruger du på transport om måneden?"))
+
+andet = int(input("Hvor meget bruger du på andre ting om måneden?"))
