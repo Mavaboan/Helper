81a04c49087d64595fa6827c604553bb1ece4637
PreussenBlue <bibbunk@gmail.com>
2019-06-03 11:32:22 +0200

    finished categories, started with comparisons

--- Main.py
+++ Main.py
@@ -1,6 +1,8 @@
+# Input, specifikt hvkellige forbrug
+
 inkomst = int(input("Hvor meget tjener du om måneden efter skat?"))
 
-husleje = int(input("Hvor meget bruger du på husleje?"))
+husleje = int(input("Hvor meget bruger du på husleje/betaling på dit hus?"))
 
 mad = int(input("Hvor meget bruger du på mad om måneden?"))
 
@@ -17,3 +19,9 @@ forsikring = int(input("Hvor meget bruger du på forsikringer om måneden?"))
 transport = int(input("Hvor meget bruger du på transport om måneden?"))
 
 andet = int(input("Hvor meget bruger du på andre ting om måneden?"))
+
+# Sammenligner inkomst med forbrug
+if (int(husleje) > (int(inkomst) * float(0.35))):
+    print("Du bruger mange penge på dit hus, overvej at finde et billigere sted at bo")
+
+if
